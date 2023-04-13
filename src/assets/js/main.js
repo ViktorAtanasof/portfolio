d6fb2248e85a793327c42abe53bbbde3c5d9b8ca
ViktorAtanasof <atanasowviktor@gmail.com>
2023-04-13 16:12:13 +0300

    Add header scroll animation

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,21 +1,34 @@
+/* Change header's background */
+function scrollHeader() {
+    const header = document.getElementById('header');
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 80) {
+        header.classList.add('scroll-header');
+    } else {
+        header.classList.remove('scroll-header');
+    }
+}
+
+window.addEventListener('scroll', scrollHeader);
+
 /* About Tabs */
 const tabs = document.querySelectorAll('[data-target]'),
     tabContents = document.querySelectorAll('[data-content]');
 
-    tabs.forEach((tab) => {
-        tab.addEventListener('click', () => {
-            const target = document.querySelector(tab.dataset.target);
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
 
-            tabContents.forEach((tabContent) => {
-                tabContent.classList.remove('tab__active');
-            });
+        tabContents.forEach((tabContent) => {
+            tabContent.classList.remove('tab__active');
+        });
 
-            target.classList.add('tab__active');
+        target.classList.add('tab__active');
 
-            tabs.forEach((tab) => {
-                tab.classList.remove('tab__active');
-            });
+        tabs.forEach((tab) => {
+            tab.classList.remove('tab__active');
+        });
 
-            tab.classList.add('tab__active');
-        })
+        tab.classList.add('tab__active');
     })
+})
